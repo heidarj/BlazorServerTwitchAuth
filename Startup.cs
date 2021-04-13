e8018599f5cea92bbe62623d8e1e3ac443be4b2d
Heiðar Hólmberg Jónsson <h@hr5.cloud>
2021-04-13 11:06:31 +0000

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,103 @@
+using BlazorServerTwitchAuth.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerTwitchAuth
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddAuthentication(options =>
+			{
+				options.DefaultScheme = "Cookies";
+				options.DefaultChallengeScheme = "oidc";
+			})
+			.AddCookie("Cookies")
+			.AddOpenIdConnect("oidc", options =>
+			{
+				options.SignInScheme = "Cookies";
+
+				options.Authority = "https://id.twitch.tv/oauth2";
+
+				options.ResponseType = "code";
+
+				options.ClientId = Configuration["Twitch:ClientId"]; ;
+				options.ClientSecret = Configuration["Twitch:ClientSecret"];
+
+				options.CallbackPath = new PathString("/signin-oidc");
+
+				string[] scopes = Configuration.GetSection("Twitch:Scopes").Get<string[]>();
+
+				options.Scope.Clear();
+
+				foreach (var scope in scopes)
+				{
+					options.Scope.Add(scope);
+				}
+
+				options.TokenValidationParameters.NameClaimType = "preferred_username";
+			});
+
+			services.AddAuthorization(options =>
+			{
+				options.FallbackPolicy = new AuthorizationPolicyBuilder()
+					.RequireAuthenticatedUser()
+					.Build();
+			});
+
+			services.AddRazorPages();
+			services.AddServerSideBlazor();
+			services.AddSingleton<WeatherForecastService>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/_Host");
+			});
+		}
+	}
+}
